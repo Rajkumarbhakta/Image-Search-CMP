8522ce569b1fd073deb7ea1442f9cc7ff95de9b7
Rajkumar <rajkumar.bhakta@sentientgeeks.com>
2025-04-28 13:17:39 +0530

    Remove unnecessary whitespace in Images data class

--- composeApp/src/commonMain/kotlin/com/rkbapps/imagesearch/model/Images.kt
+++ composeApp/src/commonMain/kotlin/com/rkbapps/imagesearch/model/Images.kt
@@ -6,11 +6,9 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Images(
-
     @SerialName("page") val page: Int? = null,
     @SerialName("per_page") val perPage: Int? = null,
     @SerialName("photos") val photos: ArrayList<Photos> = arrayListOf(),
     @SerialName("total_results") val totalResults: Int? = null,
     @SerialName("next_page") val nextPage: String? = null
-
 )
